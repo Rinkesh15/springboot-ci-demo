0974f95fc036606ff6dc2f2941b0c22e997d239c
Rinkesh Rayewaar <rinkesh.rayewaar@gmail.com>
2025-12-16 16:57:18 +0530

    Initial commit with Spring Boot project and Jenkinsfile

---
+++ src/test/java/com/example/demo/SampleTest.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+ECHO is on.
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+ECHO is on.
+public class SampleTest {
+    @Test
+    void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
