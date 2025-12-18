a6707ecd26674a8fd17bde5ccc9b4001197a0158
Rinkesh Rayewaar <rinkesh.rayewaar@gmail.com>
2025-12-18 17:53:56 +0530

    Fix SampleTest compilation issue in dev

--- src/test/java/com/example/demo/SampleTest.java
+++ src/test/java/com/example/demo/SampleTest.java
@@ -1,11 +1,13 @@
 package com.example.demo;
-ECHO is on.
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-ECHO is on.
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SampleTest {
+
     @Test
     void sampleTest() {
         assertEquals(2, 1 + 1);
     }
 }
+
