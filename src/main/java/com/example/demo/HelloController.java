ff3c521469551d66b67a5616680d9e72902ce375
Rinkesh Rayewaar <rinkesh.rayewaar@gmail.com>
2025-12-22 16:24:17 +0530

    Update Jenkinsfile and pipeline configuration

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Spring Boot CI/CD POC";
+        return "Hello from Spring Boot CI/CD POC test";
     }
 }
