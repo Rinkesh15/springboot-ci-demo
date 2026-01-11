dc8bc012f6c78ae4e01e8018bd3bc1843c5a2452
Rinkesh Rayewaar <rinkesh.rayewaar@gmail.com>
2026-01-11 21:08:27 +0530

    Test Jenkins auto-trigger from PR branch

--- src/main/java/com/autominsi/springboot_ci_demo_with_aws/controller/WordController.java
+++ src/main/java/com/autominsi/springboot_ci_demo_with_aws/controller/WordController.java
@@ -14,6 +14,7 @@ public class WordController {
     public WordController(WordService wordService) {
         this.wordService = wordService;
     }
+    // Auto-trigger test change for Jenkins CI
 
     @GetMapping("/words/{number}")
 public List<String> getWords(@PathVariable int number) {
