8c7891001260934c622cac4ab834bf532a5d8ba5
Rinkesh15 <82306543+Rinkesh15@users.noreply.github.com>
2026-01-19 12:36:34 +0530

    Modify SpringBootTest for lightweight context
    Updated SpringBootTest annotation to use a lightweight context.

--- src/test/java/com/autominsi/springboot_ci_demo_with_aws/SpringbootCiDemoWithAwsApplicationTests.java
+++ src/test/java/com/autominsi/springboot_ci_demo_with_aws/SpringbootCiDemoWithAwsApplicationTests.java
@@ -3,11 +3,11 @@ package com.autominsi.springboot_ci_demo_with_aws;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class SpringbootCiDemoWithAwsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // lightweight context test
+    }
 }
